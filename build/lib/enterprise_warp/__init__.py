12c4327fc4abec5abb6864c56560bd60036ff6f1
Yonghao Liu <liuyonghao156@163.com>
2025-12-19 13:26:45 +0800

    Remove unwanted directories

--- build/lib/enterprise_warp/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
-
-from . import enterprise_warp, discovery_warp, libstempo_warp, bilby_warp, tempo2_warp, enterprise_models, discovery_models, results, utils
